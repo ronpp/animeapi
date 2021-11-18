7c09871e21a912f9d55680a0628e553df5659053
Ronny Peña <ra.pp.91.15@gmail.com>
2021-11-18 20:56:39 +0100

    Added get anime by id and post request to add anime(without 404 error control)

--- src/main/java/com/example/animeapi/controllers/AnimeController.java
+++ src/main/java/com/example/animeapi/controllers/AnimeController.java
@@ -4,15 +4,16 @@ import com.example.animeapi.domains.dto.ListResult;
 import com.example.animeapi.domains.models.Anime;
 import com.example.animeapi.repositories.AnimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/animes")
@@ -21,7 +22,7 @@ public class AnimeController {
     private AnimeRepository animeRepository;
 
     @GetMapping(path = "/")
-    public ResponseEntity<?> getAnime(){
+    public ResponseEntity<?> getAllAnime(){
         List<Anime> animeList = animeRepository.findAll();
         if (animeList.size() != 0){
             return ResponseEntity.ok().body(ListResult.list(animeList));
@@ -29,5 +30,21 @@ public class AnimeController {
         return ResponseEntity.ok().body(ListResult.list(new ArrayList<>()));
     }
 
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?>getAnime(@PathVariable UUID id){
+        Anime anime = animeRepository.findById(id).orElse(null);
+        if(anime != null){
+            return ResponseEntity.ok().body(anime);
+        }
+        String errorMessage = String.format("{ \"message\": \"No s 'ha trobat l' anime amd id %s \"  }", id.toString());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+
+
+    @PostMapping(path = "/")
+    public ResponseEntity<?>addAnime(@RequestBody Anime anime){
+        return ResponseEntity.ok().body(animeRepository.save(anime));
+    }
 
 }
