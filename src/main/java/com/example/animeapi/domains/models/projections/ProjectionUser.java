222523e6c9039d77e5294fdc2ecf19f01643cd5b
Ronny Peña <ra.pp.91.15@gmail.com>
2021-12-20 15:37:56 +0100

    Add field favorite to ProjectionUser

--- src/main/java/com/example/animeapi/domains/models/projections/ProjectionUser.java
+++ src/main/java/com/example/animeapi/domains/models/projections/ProjectionUser.java
@@ -1,10 +1,17 @@
 package com.example.animeapi.domains.models.projections;
 
+import com.example.animeapi.domains.models.Favorite;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Set;
 import java.util.UUID;
 
 public interface ProjectionUser {
 
     UUID getUserid();
     String getUsername();
+
+    @JsonIgnoreProperties("favoriteby")
+    Set<ProjectionAnimeShort> getFavorites();
 
 }
